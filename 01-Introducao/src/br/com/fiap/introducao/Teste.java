6ad0c551e54995c7c61045405cb0e001dc2df607
Logon PF Fiap <logonpflocal@fiap.com.br>
2023-03-07 20:58:08 -0300

    Primeiro Projeto Java

---
+++ 01-Introducao/src/br/com/fiap/introducao/Teste.java
@@ -0,0 +1,22 @@
+package br.com.fiap.introducao;
+
+public class Teste {
+
+	public static void main(String args[]) {
+		System.out.println("Olá Mundo!"); 
+		
+		//Tipos primitivos
+		//Lógico
+		boolean estudando = true; //verdadeiro false //falso
+		//1 Caractere
+		char genero = 'f';
+		//Numeros inteiros
+		byte idade = 10;
+		short alturaCm = 165;
+		int quantidade = 645646;
+		long populacao = 465464646;
+		//Numeros reais 
+		float salario = 5000.50f; //f no final para dizer que eh float
+		double peso = 80.561;
+	}
+}
