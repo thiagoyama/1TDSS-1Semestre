155cc4aec269982a35857504036b3697e0d6955a
Logon PF Fiap <logonpflocal@fiap.com.br>
2023-03-21 20:52:53 -0300

    Métodos

--- 03-Metodos/src/br/com/fiap/motors/model/Carro.java
+++ 03-Metodos/src/br/com/fiap/motors/model/Carro.java
@@ -1,21 +1,44 @@
 package br.com.fiap.motors.model;
 
 public class Carro {
-
-	//Atributos
+	// Atributos
 	String modelo;
-	
+
 	int ano;
-	
+
 	double valor;
-	
-	//Metodos
-	//idade calcularIdade(ano atual)
-	public int calcularIdade(int anoAtual) {
-		//Calcular a idade
+
+	// Metodos
+	// idade calcularIdade(ano atual)
+	int calcularIdade(int anoAtual) {
+		// Calcular a idade
 		int idade = anoAtual - ano;
-		//Retornar a idade
+		// Retornar a idade
 		return idade;
 	}
 	
+	// atualizar o valor do carro
+	void atualizarValor(double porcentagem) {
+		valor *= (1 + porcentagem);
+	}
+	
+	// Criar um metodo que calcula e retorna o valor do seguro do carro
+	// Seguro do carro eh baseado no valor, 5% do valor do carro
+	double calcularSeguro() {
+		double seguro = valor * 0.05;
+		return seguro;
+	}
+
+	// Criar um metodo que recebe a quantidade de parcelas, calcula e retorna
+	//o valor da parcela do seguro
+	double calcularValorParcelaSeguro(int parcelas) {
+		double parcela = (valor * 0.05) / parcelas;
+		return parcela;
+	}
+	
 }
+
+
+
+
+
--- 03-Metodos/src/br/com/fiap/motors/model/View.java
+++ 03-Metodos/src/br/com/fiap/motors/model/View.java
@@ -16,6 +16,23 @@ public class View {
 		int i = gol.calcularIdade(2023);
 		
 		//Exibir a idade
-		System.out.println("Idade do carro: " + i);
+		System.out.println("Idade do carro: " + i + " anos");
+		
+		//Aumentar o valor do carro em 20%
+		gol.atualizarValor(0.2);
+
+		//Exibir o valor do carro
+		System.out.println("Valor do carro: " + gol.valor);
+		
+		//Calcular e exibir o valor do seguro
+		double seguro = gol.calcularSeguro();
+		System.out.println("Valor do seguro: " + seguro);
+		
+		//Exibir o valor da parcela do seguro em 3x
+		double parcela = gol.calcularValorParcelaSeguro(3);
+		System.out.println("Valor da parcela do seguro (3x): " + parcela);
+		
 	}
 }
+
+
