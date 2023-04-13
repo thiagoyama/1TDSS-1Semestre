3ab6ff3656f399cd3c76285900469df6d616a187
Thiago Yamamoto <thiagoyama@gmail.com>
2023-04-12 22:45:01 -0300

    Construtores

---
+++ 06-Construtores/src/br/com/fiap/exercicio/model/Funcionario.java
@@ -0,0 +1,54 @@
+package br.com.fiap.exercicio.model;
+
+public class Funcionario {
+
+	private String nome;
+	
+	private long matricula;
+	
+	private Profissao profissao;
+	
+	private double salario;
+
+	public Funcionario(long matricula, String nome) {
+		this.nome = nome;
+		this.matricula = matricula;
+	}
+
+	public Funcionario(long matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public long getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(long matricula) {
+		this.matricula = matricula;
+	}
+
+	public Profissao getProfissao() {
+		return profissao;
+	}
+
+	public void setProfissao(Profissao profissao) {
+		this.profissao = profissao;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+}
---
+++ 06-Construtores/src/br/com/fiap/exercicio/model/Profissao.java
@@ -0,0 +1,5 @@
+package br.com.fiap.exercicio.model;
+
+public class Profissao {
+
+}
