f3ee77f9711fa5cf423d14f9de976d1ed793cbf9
Logon PF Fiap <logonpflocal@fiap.com.br>
2023-05-02 20:54:49 -0300

    Strings

--- 08-Strings/src/br/com/fiap/strings/view/ExemploSubstring.java
+++ 08-Strings/src/br/com/fiap/strings/view/ExemploSubstring.java
@@ -13,18 +13,37 @@ public class ExemploSubstring {
 		System.out.println(primeiroNome);		
 		
 		//Exibir o sobrenome
+		String sobrenome = nome.substring(nome.indexOf(" ")+1);
+		System.out.println(sobrenome);
 		
 		//Ler uma data (dd/mm/aaaa)
+		String data = JOptionPane.showInputDialog("Digite uma data");
 		
 		//Exibir o dia
+		String dia = data.substring(0, data.indexOf("/"));
+		System.out.println(dia);
 		
 		//Exibir o mês 
+		String mes = data.substring(data.indexOf("/")+1,
+				data.lastIndexOf("/"));
+		System.out.println(mes);
 		
 		//Exibir o ano
+		String ano = data.substring(data.lastIndexOf("/")+1);
+		System.out.println(ano);
 		
 		//Ler um e-mail
+		String email = JOptionPane.showInputDialog("Digite um e-mail");
 		
+		//thiago.yamamoto@fiap.com.br -> fiap
 		//Exibir o dominio do email
+		String dominio = email
+				.substring(email.indexOf("@")+1);
+		
+		String dominio2 = dominio.substring(0, 
+				dominio.indexOf("."));
+		
+		System.out.println(dominio2);
 		
 	}//main
 }//class
---
+++ 08-Strings/src/br/com/fiap/strings/view/Maiuscula.java
@@ -0,0 +1,20 @@
+package br.com.fiap.strings.view;
+
+import javax.swing.JOptionPane;
+
+public class Maiuscula {
+
+	public static void main(String[] args) {
+		String algo = JOptionPane.showInputDialog("Digite algo");
+		System.out.println(algo.toUpperCase());
+	}
+	
+}
+
+
+
+
+
+
+
+
